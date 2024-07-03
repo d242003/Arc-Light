0641a59b7f47a350aaebdfc0cca99bdfbbf7d1c0
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-03 16:08:46 +0530

    feat: setup base project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import Home from "./routes/home";
+import EasyMedHard from "./routes/easy-med-hard";
+import Dashboard from "./routes/dashboard";
+import Languages from "./routes/languages";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/setup",
+        element: <EasyMedHard />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/languages",
+        element: <Languages />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
