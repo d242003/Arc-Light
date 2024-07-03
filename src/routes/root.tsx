9b89c212f7fccf0770f5af5f890f3df0dbd02e53
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-03 21:08:17 +0530

    add full site

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
-    <div className="font-abeezee">
+    <div className="font-abeezee bg-[#EEEEEE] min-h-screen">
       <Outlet />
     </div>
   );
